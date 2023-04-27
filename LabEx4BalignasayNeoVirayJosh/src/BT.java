cacdbd7b55439ee34bd101f254f63bd97a7b51ed
Neo Balignasay <neobalignasay@gmail.com>
2023-04-27 11:39:10 +0800

    Update BT.java

--- LabEx4BalignasayNeoVirayJosh/src/BT.java
+++ LabEx4BalignasayNeoVirayJosh/src/BT.java
@@ -55,7 +55,7 @@ public class BT<T> extends BTNode<T>{
             return "ht=" + --height + " " + root.toString();
     }
 
-    public int countLeavesPostorder(BTNode left) {
+    public int countLeavesPostorder(BTNode<T> left) {
         if (left == null) {
             return 0;
         }
